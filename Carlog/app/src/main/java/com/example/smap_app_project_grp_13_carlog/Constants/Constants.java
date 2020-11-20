72b78f170c526864f5a9b3b357f17756743b154f
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-11-21 00:03:20 +0100

    Added comment in Constants

--- Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Constants/Constants.java
+++ Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Constants/Constants.java
@@ -2,6 +2,11 @@ package com.example.smap_app_project_grp_13_carlog.Constants;
 
 public class Constants {
 
+    //Denne constant kom fra et tidligere push
+    public String ID = "ID";
+    //Hvad er den og bruges den? ikke sat op rigtigt. brug "psfs" når i skriver constants
+
+
     //Logging tags
     public static final String REPOTAG = "REPOSOTORYTAG";
 }
