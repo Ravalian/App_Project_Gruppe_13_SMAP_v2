8b8be352fe83977e763fa18eb3b6dceb2013a5ae
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-11-20 13:28:26 +0100

    Auth
    Login works!

--- Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Activities/LoginActivity.java
+++ Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Activities/LoginActivity.java
@@ -1,16 +1,126 @@
 package com.example.smap_app_project_grp_13_carlog.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.smap_app_project_grp_13_carlog.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 8372;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+        if(mAuth == null){
+            mAuth = FirebaseAuth.getInstance();
+        }
+        if(mAuth.getCurrentUser()!=null){
+            gotoUserInterface();
+        }
+
+    }
+
+    private void signOut() {
+        Log.d("logout: onComplete", "Do something ");
+        if(mAuth == null){
+            mAuth = FirebaseAuth.getInstance();
+        }
+        if(mAuth.getCurrentUser()!=null) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "Not Logged In", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void signIn(){
+        if(mAuth == null){
+            mAuth = FirebaseAuth.getInstance();
+        }
+        if(mAuth.getCurrentUser()!=null){
+            gotoUserInterface();
+        }else {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(), RC_SIGN_IN
+            );
+        }
+    }
+
+    private void gotoUserInterface() {
+        Intent i = new Intent(this, UserInterfaceActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = mAuth.getCurrentUser().getUid();
+                Toast.makeText(this, "User Logged In\n" +uid, Toast.LENGTH_SHORT).show();
+                gotoUserInterface();
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
