1e0a6758822d02f8bf2d53cdd32a2089bdbd2ad3
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-11-30 15:54:50 +0100

    Added functionality to be able to upload images from the phones gallery or take a picture(not sure if works yet) Then added request permission functionality to access camera/storage on phone. More strings I guess.

--- Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Constants/Constants.java
+++ Carlog/app/src/main/java/com/example/smap_app_project_grp_13_carlog/Constants/Constants.java
@@ -2,9 +2,15 @@ package com.example.smap_app_project_grp_13_carlog.Constants;
 
 public class Constants {
 
+    //Request Codes
+    public static final int REGISTERVHACTSELECTIMGOPTION1 = 401;
+    public static final int REGISTERVHACTSELECTIMGOPTION2 = 402;
+    public static final int REGISTERVHACTREQUESTPERMISSON = 499;
+
     //Logging tags
     public static final String REPOTAG = "REPOSOTORYTAG";
     public static final String REGISTEREDTAG = "REGISTEREDTAG";
+    public static final String REGISTERVHACTTAG = "RegisterVehicleTag";
 
     //Intent tags
     public static final String ID = "ID";
